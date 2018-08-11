8e2c7217440b22cc599492e69aa93970bff02fee
Jacob Ingalls <jacobingalls@yahoo.com>
2018-08-10 22:00:08 -0700

    Inital roads commit

---
+++ Assets/RoadManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int y = 0; y < 10; y ++) {
+            for (int x = 0; x < 10; x++)
+            {
+                var go = Instantiate(prefab);
+                go.transform.position = new Vector3(x, y, 0.0f);
+            } 
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
