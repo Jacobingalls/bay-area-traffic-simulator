dd96fe66839f8bffe193c61becd215da3676ce83
Morgan Cabral <morgan.cabral@gmail.com>
2018-08-12 02:41:30 -0700

    Initial progress on the Building subsystem. This will also require building out a Resource management system for electricty, water draw, etc. along with some kind of scoring/win-lose system...

---
+++ Assets/Buildings/Data/BuildingData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BuildingData : UpdateableData
+{
+    [Header("Building Manager State")]
+
+    // The list of models which should be used for residential buildings.
+    public GameObject[,] residentialBuildings;
+
+    // The list of models that should be used for commercial buildings.
+    public GameObject[,] commercialBuildings;
+
+    // The list of models that should be used for industrial buildings.
+    public GameObject[,] industrialBuildings;
+
+    // A handy value for scaling building models up or down. 
+    public float globalBuildingScale = 1.0f;
+}
